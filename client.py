d60e2b8da5698bef3f9b485084c7be3897ddb45b
Julian Stecklina <js@alien8.de>
2013-05-28 21:54:01 +0200

    Added License (AGPL).

--- client.py
+++ client.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python2
 
+# Command Line Notify Python Client
+# Copyright (C) 2013 Julian Stecklina <js@alien8.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
 
 import sys
@@ -19,6 +35,7 @@ from oauth2client.tools  import run
 
 # We get the discovery document from this URL. The apiclient.discovery
 # module will do the magic of generating a client backend for us.
+# XXX Cache this.
 service_discovery_url = "https://cmdlinenotify.appspot.com/_ah/api/discovery/v1/apis/jobapi/v1/rest"
 
 # From the API console.
